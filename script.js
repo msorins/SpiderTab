df1e3897eb1fa75743c331c7dccc1d8011e93b24
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-12-22 11:38:48 +0100

    Redirecting to new page accordingly

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-
 var websites = ['https://www.producthunt.com/', 'https://www.google.ro/'];
 var chosenWebsiteIndex = getRandomInt(websites.length);
 var chosenWebsite = websites[chosenWebsiteIndex];
 
 console.log(`Chosen website is ${chosenWebsite}`);
+window.location.href = chosenWebsite;
