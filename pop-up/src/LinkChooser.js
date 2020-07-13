deb57c20b36b464df96bc00b96ef4ee3a86e5b75
Mircea Sorin-Sebastian <mircea.sorin.sebastian@gmail.com>
2020-07-13 16:32:42 -0700

    design updates

--- pop-up/src/LinkChooser.js
+++ pop-up/src/LinkChooser.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Form, Input, Icon, Button } from 'antd';
+import React from "react";
+import { Form, Input, Icon, Button } from "antd";
 
 class LinkChooser extends React.Component {
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
-  remove = k => {
+  increaseWidth = (k) => {
+    console.log("--> increase: ", k);
+  };
+
+  decreaseWidth = (k) => {
+    console.log("--> decrease: ", k);
+  };
+
+  remove = (k) => {
     const { form } = this.props;
     // can use data-binding to get
-    const keys = form.getFieldValue('keys');
+    const keys = form.getFieldValue("keys");
     // We need at least one passenger
     if (keys.length === 1) {
       return;
@@ -16,93 +23,114 @@ class LinkChooser extends React.Component {
 
     // can use data-binding to set
     form.setFieldsValue({
-      keys: keys.filter(key => key !== k),
+      keys: keys.filter((key) => key !== k),
     });
 
-    this.persistToLocalStorage()
+    this.persistToLocalStorage();
   };
 
   add = () => {
     const { form } = this.props;
     console.log(JSON.stringify(this.props));
     // can use data-binding to get
-    const keys = form.getFieldValue('keys');
+    const keys = form.getFieldValue("keys");
     const nextKeys = keys.concat(keys.length);
     // can use data-binding to set
     // important! notify form to detect changes
     form.setFieldsValue({
       keys: nextKeys,
     });
-
   };
 
   persistToLocalStorage = () => {
     this.props.form.validateFields((err, values) => {
       if (!err) {
         const { keys, names } = values;
-        console.log('Saving to local storage:', keys.map(key => names[key]));
+        console.log(
+          "Saving to local storage:",
+          keys.map((key) => names[key])
+        );
 
-        window.localStorage.setItem('spider-web-list', JSON.stringify(keys.map(key => names[key])));
+        window.localStorage.setItem(
+          "spider-web-list",
+          JSON.stringify(keys.map((key) => names[key]))
+        );
       }
     });
-  }
+  };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
-    this.persistToLocalStorage()
+    this.persistToLocalStorage();
   };
 
   render() {
     const { getFieldDecorator, getFieldValue } = this.props.form;
 
-    getFieldDecorator('keys', { initialValue: [] });
-    const keys = getFieldValue('keys');
+    getFieldDecorator("keys", { initialValue: [] });
+    const keys = getFieldValue("keys");
     const formItems = keys.map((k, index) => (
-      <Form.Item
-      >
-        {getFieldDecorator(`names[${k}]`, {
-          validateTrigger: ['onChange', 'onBlur'],
-          rules: [
-            {
-              required: true,
-              whitespace: true,
-              message: "Enter the link you want to be opened",
-            },
-          ],
-        })(<Input placeholder="url" style={{ width: '85%', marginLeft: '4%', height: '20' }} />)}
-        {/* <FastForwardFilled /> */}
-        <Icon
-            type="zoom-in"
-            onClick={() => this.remove(k)}
-            style={{ marginLeft: 7, 'fontSize': 18 }}
-          />
-         <Icon
-            className="dynamic-delete-button"
-            type="zoom-out"
-            onClick={() => this.remove(k)}
-            style={{ marginLeft: 7, marginRight: 6, 'fontSize': 18 }}
-          /> 
-        {keys.length > 1 ? (
-          <span>
-            <span style={{'width': '10px', 'fontSize': 25}}>|</span>
+      <Form.Item>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          {getFieldDecorator(`names[${k}]`, {
+            validateTrigger: ["onChange", "onBlur"],
+            rules: [
+              {
+                required: true,
+                whitespace: true,
+                message: "Enter the link you want to be opened",
+              },
+            ],
+          })(
+            <Input
+              placeholder="url"
+              style={{ width: "75%", marginLeft: "2%", height: "20" }}
+            />
+          )}
+          <div>
             <Icon
-            className="dynamic-delete-button"
-            type="delete"
-            onClick={() => this.remove(k)}
-            style={{ marginLeft: 7, 'fontSize': 18}}
-          />
-          </span>
-        ) : null}
+              type="zoom-in"
+              onClick={() => this.decreaseWidth(k)}
+              style={{ marginLeft: 7, fontSize: 18 }}
+            />
+            <Icon
+              className="dynamic-delete-button"
+              type="zoom-out"
+              onClick={() => this.increaseWidth(k)}
+              style={{ marginLeft: 7, fontSize: 18 }}
+            />
+            {keys.length > 1 ? (
+              <span>
+                <Icon
+                  className="dynamic-delete-button"
+                  type="delete"
+                  onClick={() => this.remove(k)}
+                  style={{ marginLeft: 7, fontSize: 18, color: 'black' }}
+                />
+              </span>
+            ) : null}
+          </div>
+        </div>
       </Form.Item>
     ));
 
     return (
       <Form onSubmit={this.handleSubmit} onChange={this.handleSubmit}>
-        <div style={{height: 7}}></div>
+        <div style={{ height: 7 }}></div>
         {formItems}
         <Form.Item>
-          <Button type="dashed" onClick={this.add} style={{ width: '94%', marginLeft: '2%' }}>
-            <Icon type="plus" /> Add field
+          <Button
+            type="dashed"
+            onClick={this.add}
+            style={{ width: "94%", marginLeft: "2%" }}
+          >
+            <Icon type="plus" /> Add Website
           </Button>
         </Form.Item>
       </Form>
